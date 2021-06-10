e74cc0cebfdcbec12475b9502aeb4c00c60edd5e
Brian P. Mucha <brian@brianpmucha.com>
2021-06-10 13:38:14 -0500

    Update jQuery.madTopTeams.js
    Improved IE support.

--- dist/scripts/jQuery.madTopTeams.js
+++ dist/scripts/jQuery.madTopTeams.js
@@ -1,12 +1,11 @@
-
 /**********************************************************************\
 *
 *	jQuery.madTopTeams
 *	----------------------
-*	version: 2.0.3
-*	date: 2020/08/19
+*	version: 2.0.5
+*	date: 2021/06/10
 *	license: GPL-3.0-or-later
-*	copyright (C) 2020 Brian Patrick Mucha
+*	copyright (C) 2021 Brian Patrick Mucha
 *
 \**********************************************************************/
 
@@ -15,21 +14,25 @@
 * <div class="section">
 *   <div class="container">
 *     <h2>Top Teams</h2>
-*     <div id="top_teams_results" class="row list-results"></div>
+*     <div id="team_list_results" class="row list-results">
+*     </div>
 *   </div>
 * </div>
-* 
+*
 * <!--// Results Template //-->
-* <script id="results_template_top_teams" type="text/template">
-*   <div class="col-12 col-lg-8">
-*     <strong>%%name%%</strong>
-*   </div>
-*   <div class="col-12 col-lg-4 text-right">
-*     <em>%%total%%</em>
+* <script id="team_item_template" type="text/template">
+*   <div class="col-12">
+*     <div class="row shaded">
+*       <div class="col-12 col-md-8">
+*         <strong>%%name%%</strong>
+*       </div>
+*       <div class="col-12 col-md-4 d-flex align-items-right">
+*         %%total%%
+*       </div>
+*     </div>
 *   </div>
 * </script>
-* 
-* <!--// Init Top Teams //-->
+*
 * <script>
 *  jQuery(document).ready(function ($) {
 *    var options =
@@ -39,16 +42,20 @@
 *      "secureConvioPath":"https://[[S29:SECURE_DOMAIN]][[S29:SECURE_PATH]]",
 *      "apiKey":"[[S0:CONVIO_API_KEY]]",
 *      "fr_ids":["1234", "5678"],
-*      "maxCount": "10",
+*      "maxCount": 10,
+*      "folderPath":"/Events/CommunityFundraising",
 *      "loadingImage": "../images/loader.gif",
 *      "loadingImageAlt": "&#x1F551",
-*      "results_template_id": "results_template_top_teams"
+*      "results_template_id": "team_item_template"
 *    }
-*   $("#top_teams_results").madTopTeams( options );
+*   $("#team_list_results").madTopTeams( options );
 *  });
 * </script>
++
+* Note: Links are only generated for SINGLE TEAMRAISER lists.
 *
 \**********************************************************************/
+
 (function ($) {
 
 	"use strict";
@@ -56,7 +63,7 @@
 	/* ********** Private Functions ********** */
 
 	function markLoading(element, settings) {
-		$(element).empty();
+		$(".search_item").remove();
 		if (settings.loadingImage && settings.loadingText) {
 			element.append("<div class=\"list_loading\"><img alt=\"" + settings.loadingImageAlt + "\" src=\"" + settings.loadingImage + "\" />" + settings.loadingText + "</div>");
 		} else if (settings.loadingImage) {
@@ -93,7 +100,7 @@
 			{
 				var compiledResults = [];
 				$.each(results, function( index, value ) {
-					if(value.getTopTeamsDataResponse) {
+					if(value.getTopTeamsDataResponse && value.getTopTeamsDataResponse.teamraiserData) {
 						var counter;
 						for (counter = 0; counter < value.getTopTeamsDataResponse.teamraiserData.length; counter++) {
 							value.getTopTeamsDataResponse.teamraiserData[counter].total_num = Number(value.getTopTeamsDataResponse.teamraiserData[counter].total.replace(/[^0-9.-]+/g,""));
@@ -113,12 +120,16 @@
 	function updateDomElement(data, element, settings) {
 		if( data.errorResponse )
 		{
-			element.html("<div class=\"list_message\">(" + data.errorResponse.message + ")</div>");
+			element.html("<div class=\"search_loading\">(" + data.errorResponse.message + ")</div>");
 		} else {
 			$(element).empty();
 			var trObject = ensureArray(data);
 			if ($(trObject).length > 0) {
-				trObject.sort((a,b)=>b.total_num-a.total_num);
+				trObject.sort(function(a, b) {
+					if (a.total_num < b.total_num) return 1;
+					if (a.total_num > b.total_num) return -1;
+					return 0;
+				});
 				$.each(trObject, function (idx, val) {
 
 					var template = document.getElementById(settings.results_template_id);
@@ -129,23 +140,29 @@
 					var id;
 					var name;
 					var total;
+					var link;
 
 					id = this.id;
 					name = this.name;
 					total = this.total;
 
+					if (settings.fr_ids.length == 1) {
+						link = settings.nonsecureConvioPath + "TR" + (settings.folderPath ? settings.folderPath : "") + "?team_id=" + this.id + "&pg=team&fr_id=" + settings.fr_ids[0];
+					}
+
 					newEntry = templateHtml
 						.replace(/%%id%%/g, id)
 						.replace(/%%name%%/g, name)
-						.replace(/%%total%%/g, total);
+						.replace(/%%total%%/g, "$"+total)
+						.replace(/%%link%%/g, link);
 
 					element.append(newEntry);
 
-					if(idx+1 == settings.maxCount) { return false; }
+					if(idx == settings.maxCount) { return false; }
 
 				});
 			} else {
-				element.html("<div class=\"list_message\">(end of list)</div>");
+				element.html("<div class=\"search_loading\">(end of list)</div>");
 			}
 			if (settings.callBack) { settings.callBack(); }
 		}
@@ -168,6 +185,7 @@
 	{
 		"fr_ids": [],
 		"maxCount": 10,
+		"folderPath": null,
 		"loadingImage": null,
 		"loadingImageAlt": null,
 		"loadingText": null,
